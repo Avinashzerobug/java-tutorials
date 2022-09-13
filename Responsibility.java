356aed192dd22ef9823e57145b71ef75e5e5e48b
Manish8951 <manish.xworkz@gmail.com>
2022-09-13 11:10:00 +0530

    method call

---
+++ Responsibility.java
@@ -0,0 +1,133 @@
+class Responsibility{
+
+     static void Age()
+	 {
+		 
+	    System.out.println("Age wise responsibility");
+		int age=10;
+		
+		if(age==11)
+		{
+			 System.out.println("school");
+			
+		}
+		else
+		{
+			 System.out.println("still a child");
+			 return;
+		}
+		 
+		int age1=16;
+		
+		if(age1<=16)
+		{
+			 System.out.println("college");
+		}
+		else
+		{
+			 System.out.println("none");
+		}
+	    int age2=18;
+		
+		if(age2>=18)
+		{
+			 System.out.println("eligible for voting");
+			  System.out.println("driving lisence");
+		}
+			 String gender="f";
+			  if(gender=="f")
+			  {
+			   System.out.println("marriage");
+			  }
+		int age3=21;
+		if(age3==21 || age3<=22 || age3>=22) 
+		{
+			 System.out.println("marriage for boys");
+			  System.out.println("juice");
+		}
+	    else
+		{
+			 System.out.println("no juice no marriage");
+			 
+		}
+	    int age4=22;
+		if(age4==23)
+		{
+			 System.out.println("graduate");
+		}
+		else
+		{
+			 System.out.println("backlog student");
+		}
+		int age5=25;
+		if(age5==25)
+		{
+			 System.out.println("job");
+		}
+		else
+		{
+			 System.out.println("lazy guy");
+		}
+		int age6=28;
+		if(age6==28)
+		{
+			 System.out.println("kids");
+		}
+		else
+		{
+			 System.out.println("no kids");
+		}
+		int age7=30;
+		if(age7==30)
+		{
+			 System.out.println("another kid");
+		}
+		else
+		{
+			 System.out.println("one kid is good for now...no need hurry");
+		}
+	 int age8=45;
+	 if(age8==45)
+	 {
+		  System.out.println("kids future");
+	 }
+	 else
+	 {
+		  System.out.println("make youre future better first");
+	 }
+	 int age9=50;
+	 if(age9==50)
+	 {
+		  System.out.println("kids education");
+	 }
+	 else
+     {
+		  System.out.println("make money");
+	 }
+	 int age10=58;
+	 if(age10==58)
+	 {
+		  System.out.println("retired");
+	 }
+	 else
+	 {
+		  System.out.println("no need  to retired");
+	 }
+	 int age11=60;
+	 if(age11==60)
+	 {
+		  System.out.println("grand kids");
+	 }
+	 else
+	 {
+		  System.out.println("there is problem");
+	 }
+	 int age12=70;
+	 if(age11==70)
+	 {
+		  System.out.println("life goes on");
+	 }
+	
+	 }
+
+}

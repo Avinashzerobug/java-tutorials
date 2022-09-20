4475139eecf3a5c1f7f66a792797df6ba0db4d62
Manish8951 <manish.xworkz@gmail.com>
2022-09-21 00:01:21 +0530

    message test

--- Tablets.java
+++ Tablets.java
@@ -1,42 +1,37 @@
 class Tablets
     {
+	 static	String[] chemical;
+	 static	int[] noOfCompanies;
+	 static	String[] countrySupply;
+	 static	int[] supplyCompanies;
+	 static	String[] productionInKarnataka;
+	 static	double[] contentOfChemicals;
+	 static	String[] typeOfMachineUsed;
+	 static	int[] weight;
+	 static	String[] effects;
+	 static	String[] guideLines;
+	 static	String[] noOfTablets;
     static String[] color;
 	static String typeOftablet;
-	static int  weight;
-	static double height;
-	static int width;
-	static String chemical;
-	static int noOfTablets;
-	static int tabletCost;
-	static String Company;
-	static int Strength;
-	static String effects;
-	static String brand;
-	static String treatment;
-	static int storage;
-	static String Class;
-	static String packet;
-	static String quality;
+	
 	
 	static void printDetails()
 	{
+		
+		System.out.println (chemical);
+		System.out.println (noOfCompanies);
+        System.out.println(countrySupply);
+		System.out.println (supplyCompanies);
+		System.out.println (productionInKarnataka);
+		System.out.println (contentOfChemicals);
+		System.out.println (typeOfMachineUsed);
+		System.out.println (weight);
+		System.out.println (effects);
+		System.out.println (guideLines);
+		System.out.println (noOfTablets);
+			
     System.out.println(color);
-	System.out.println(typeOftablet);
-	System.out.println(weight);
-	System.out.println(height);
-    System.out.println(width);
-	System.out.println(chemical);
-	System.out.println(noOfTablets);
-	System.out.println(tabletCost);
-	System.out.println(Company);
-	System.out.println(Strength);
-	System.out.println(effects);
-	System.out.println(brand);
-	System.out.println(treatment);
-	System.out.println(storage);
-	System.out.println(Class);
-	System.out.println(packet);
-	System.out.println(quality);
+	
  
     if(color!=null)
 	{
@@ -47,7 +42,117 @@ class Tablets
 	System.out.println(ref+"color");
 	}
 		  
-	}	 
+	}	
+if(chemical!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<chemical.length;index++)
+	{
+	String ref1=chemical[index];
+	System.out.println(ref1+"chemical1");
+	}
+		  
+	}	
+if(noOfCompanies!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<noOfCompanies.length;index++)
+	{
+	int ref2=noOfCompanies[index];
+	System.out.println(ref2+"noOfCompanies");
+	}
+		  
+	}	
+if(countrySupply!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<countrySupply.length;index++)
+	{
+	String ref3=countrySupply[index];
+	System.out.println(ref3+"countrySupply");
+	}
+		  
+	}	
+if(supplyCompanies!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<supplyCompanies.length;index++)
+	{
+	int ref4=supplyCompanies[index];
+	System.out.println(ref4+"supplyCompanies");
+	}
+		  
+	}	
+if(productionInKarnataka!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<productionInKarnataka.length;index++)
+	{
+	String ref5=productionInKarnataka[index];
+	System.out.println(ref5+"productionInKarnataka");
+	}
+		  
+	}	
+if(contentOfChemicals!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<contentOfChemicals.length;index++)
+	{
+	double ref6=contentOfChemicals[index];
+	System.out.println(ref6+"contentOfChemicals");
+	}
+		  
+	}	
+if(typeOfMachineUsed!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<typeOfMachineUsed.length;index++)
+	{
+	String ref7=typeOfMachineUsed[index];
+	System.out.println(ref7+"typeOfMachineUsed");
+	}
+		  
+	}	
+if(weight!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<weight.length;index++)
+	{
+	int ref8=weight[index];
+	System.out.println(ref8+"weight1");
+	}
+		  
+	}	
+if(effects!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<effects.length;index++)
+	{
+	String ref9=effects[index];
+	System.out.println(ref9+"effects");
+	}
+		  
+	}	
+if(guideLines!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<guideLines.length;index++)
+	{
+	String ref10=guideLines[index];
+	System.out.println(ref10+"guideLines");
+	}
+		  
+	}	
+if(noOfTablets!=null)
+	{
+    System.out.println("Array is pointing to the memory");
+	for(int index=0;index<noOfTablets.length;index++)
+	{
+	String ref11=noOfTablets[index];
+	System.out.println(ref11+"noOfTablets");
+	}
+		  
+	}	
 
 	}
     }
--- TabletsRunner.java
+++ TabletsRunner.java
@@ -5,28 +5,33 @@ class TabletsRunner{
    Tablets.printDetails();
    
    System.out.println("*********************************************");
-   
-   String[] color1={"Blue","Green","Orange","Yellow","Purple","Brown","Gray","Red","Maroon","Violet","Charcol","Bronze","gold","Silver","Pink"};
-   Tablets.typeOftablet="Dolo";
-   Tablets.weight=2;
-   Tablets.height=1; 
-   Tablets.width=3;
-   Tablets.chemical="Acetaminophen";
-   Tablets.noOfTablets=15;
-   Tablets.tabletCost=40;
-   Tablets.Company="Micro labs ltd";
-   Tablets.Strength=650;
-   Tablets.effects="vomiting";
-   Tablets.brand="Dolo";
-   Tablets.treatment="Fever";
-   Tablets.storage=25;
-   Tablets.Class="Analgesic";
-   Tablets.packet="Recycled";
-   Tablets.quality="MicroLabs";
-   
-   System.out.println("*********************************************");
-	  
+ 
+String[] color1={"Blue","Green","Orange","Yellow","Purple","Brown","Gray","Red","Maroon","Violet","Charcol","Bronze","gold","Silver","Pink"};
+ String[] chemical1={"acetic anhydride", "acetone", "diethyl ether"," benzyl chloride"," toluene"," hydrochloric acid"," Botulinum","Cyanide","Arsenic","Strychnine","Carbon Tetraklorida"};
+ int[] noOfCompanies1={8,7,5,9,2,4,33,66,2,6,3,2,5,};
+ String[] countrySupply1={"USA","Russia","Japan","Isreal","Ukraine","Bangladesh","Austrailia","Shrilanka","Nepal","Maymnar","Philippenes","England","Saudi Arabia"};
+int[] supplyCompanies1={22,55,19,33,55,77,44,77,11,22,14,4,78};
+ String[] productionInKarnataka1={"Bagalkot","Bangalore","Hubli","Tumkur","Belagavi","Vijayapur","Shivmogga","Mangalore","Ballari","Kalburgi","Dharwad"};
+double[] contentOfChemicals1={1.2,1.0,3.2,2.5,5.5,25.5,12.5,0.9,1.1,36.4,14,13.5};
+ String[] typeOfMachineUsed1={"Mixing","Cleaning","containing","Producing","Storing","Testing","Transporting","Analysing","Adding","Stamping the bill","Storing cold in climate"};
+ int[] weight1={2,1,5,3,0,1,3,4,6,4,6,3,6,};
+ String[] effects1={"Stomach pain","Indigestion","Malaise","Diffuse pain","Nausea","Vomiting","Dizzness","Diarrhoea","Drowsiness"};
+ String[] guideLines1={"take madicine with small amount of water","read prescribed","dont take over dose","take advise from doctor"};
+String[]  noOfTablets1={"Tablet","Capsule","Granules","Sachet","Lozenges","Pill","Inhaler","Powder","Chewable Tablets"};
+  
+  
    Tablets.color=color1;
+    Tablets.chemical=chemical1;
+	 Tablets.noOfCompanies=noOfCompanies1;
+	  Tablets.countrySupply=countrySupply1;
+	   Tablets.supplyCompanies=supplyCompanies1;
+	    Tablets.productionInKarnataka=productionInKarnataka1;
+		 Tablets.contentOfChemicals=contentOfChemicals1;
+		  Tablets.typeOfMachineUsed=typeOfMachineUsed1;
+		   Tablets.weight=weight1;
+		    Tablets.effects=effects1;
+			 Tablets.guideLines=guideLines1;
+			  Tablets.noOfTablets=noOfTablets1;
    Tablets.printDetails();
    }
    

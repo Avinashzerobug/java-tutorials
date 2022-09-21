c61f1e99daffb10186866e7f61bca7b20343f9c3
Manish8951 <manish.xworkz@gmail.com>
2022-09-21 18:48:17 +0530

    message test

---
+++ GroceriesRunner.java
@@ -0,0 +1,91 @@
+class GroceriesRunner{
+
+ public static void main(String[]  Shopping)
+ {
+      Groceries.setrooms(140);
+      Groceries.setvegitable("Onion");
+      Groceries.setname("Walmart");
+      Groceries.settypeOfItems(200);
+      Groceries.setlocation("Bagalkot");
+      Groceries.setorder("Available");
+      Groceries.setnoOfBaskets(2000);
+      Groceries.setcurrentBill(24535.08);
+      Groceries.setshopOwner("Sam Walton");
+      Groceries.setfacility("Free Delivery");
+      Groceries.setbranch("Bangalore");
+      Groceries.setnoOfBranch(200);
+      Groceries.setonlineOrder("Available to track");
+      Groceries.setnoOfFloors(10);
+      Groceries.setonlinePayment("20% off on online payment");
+  
+      int ref=Groceries.getrooms();
+	  System.out.println(ref);
+	  
+	  String ref1=Groceries.getvegitable();
+	  System.out.println(ref1);
+	  
+	  String ref2=Groceries.getname();
+	  System.out.println(ref2);
+	  
+	  int ref3=Groceries.gettypeOfItems();
+	  System.out.println(ref3);
+	  
+	  String ref4=Groceries.getlocation();
+	  System.out.println(ref4);
+	  
+	  String ref5=Groceries.getorder();
+	  System.out.println(ref5);
+	  
+	  int ref6=Groceries.getnoOfBaskets();
+	  System.out.println(ref6);
+	  
+	  double ref7=Groceries.getcurrentBill();
+	  System.out.println(ref7);
+	  
+	  String ref8=Groceries.getshopOwner();
+	  System.out.println(ref8);
+	  
+	  String ref9=Groceries.getfacility();
+	  System.out.println(ref9);
+	  
+	  String ref10=Groceries.getbranch();
+	  System.out.println(ref10);
+	  
+	  int ref11=Groceries.getnoOfBranch();
+	  System.out.println(ref11);
+	  
+	  String ref12=Groceries.getonlineOrder();
+	  System.out.println(ref12);
+	  
+	  int ref13=Groceries.getnoOfFloors();
+	  System.out.println(ref13);
+	  
+	 int ref14=Groceries.getrooms();
+	  System.out.println(ref14);
+ 
+ 
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ polymorphism/Groceries.java
@@ -0,0 +1,154 @@
+class Groceries{
+
+  static int     rooms;
+  static String  vegitable;
+  static String  name;
+  static int     typeOfItems;
+  static String  location;
+  static String  order;
+  static int     noOfBaskets;
+  static double  currentBill;
+  static String  shopOwner;
+  static String  facility;
+  static String  branch;
+  static int     noOfBranch;
+  static String  onlineOrder;
+  static int     noOfFloors;
+  static String  onlinePayment;
+
+   static void setrooms(int value)
+   {
+	   rooms=value;
+   }
+   static int getrooms()
+   {
+	   return rooms;
+   }
+   
+   static void setvegitable(String value1)
+   {
+	   vegitable=value1;
+   }
+   static String getvegitable()
+   {
+	   return vegitable;
+   }
+   
+    static void settypeOfItems(int value2)
+   {
+	   typeOfItems=value2;
+   }
+   static int gettypeOfItems()
+   {
+	   return typeOfItems;
+   }
+   
+    static void setname(String value3)
+   {
+	   name=value3;
+   }
+   static String getname()
+   {
+	   return name;
+   }
+   
+   static void setlocation(String value4)
+   {
+	   location=value4;
+   }
+   static String getlocation()
+   {
+	   return location;
+   }
+   
+    static void setorder(String value5)
+   {
+	   order=value5;
+   }
+   static String getorder()
+   {
+	   return order;
+   }
+   
+     static void setnoOfBaskets(int value6)
+   {
+	   noOfBaskets=value6;
+   }
+   static int getnoOfBaskets()
+   {
+	   return noOfBaskets;
+   }
+   
+    static void setcurrentBill(double value7)
+   {
+	   currentBill=value7;
+   }
+   static double getcurrentBill()
+   {
+	   return currentBill;
+   }
+   
+    static void setshopOwner(String value8)
+   {
+	   shopOwner=value8;
+   }
+   static String getshopOwner()
+   {
+	   return shopOwner;
+   }
+   
+   static void setfacility(String value9)
+   {
+	   facility=value9;
+   }
+   static String getfacility()
+   {
+	   return facility;
+   }
+
+    static void setbranch(String value10)
+   {
+	   branch=value10;
+   }
+   static String getbranch()
+   {
+	   return branch;
+   }
+
+   static void setnoOfBranch(int value11)
+   {
+	   noOfBranch=value11;
+   }
+   static int getnoOfBranch()
+   {
+	   return noOfBranch;
+   }
+
+    static void setonlineOrder(String value12)
+   {
+	   onlineOrder=value12;
+   }
+   static String getonlineOrder()
+   {
+	   return onlineOrder;
+   }
+   
+   static void setnoOfFloors(int value13)
+   {
+	   noOfFloors=value13;
+   }
+   static int getnoOfFloors()
+   {
+	   return noOfFloors;
+   }
+
+   static void setonlinePayment(String value14)
+   {
+	   onlinePayment=value14;
+   }
+   static String getonlinePayment()
+   {
+	   return onlinePayment;
+   }
+ 
+}
